4ae6eb872407e6bb437bed6580e40f74f1786af7
joshruff <joshruff@tamu.edu>
2018-06-20 09:43:05 -0500

    Loop now controls voltage and pulls phase from analyzer

--- ps_control/ps_cal.py
+++ ps_control/ps_cal.py
@@ -1,23 +1,48 @@
 import ps_control.network_analyzer as network_analyzer
 import serial
+import numpy as np
+from time import sleep
 
-pna_address = ('165.91.209.114',5025)
+
+pna_address = ('10.0.0.3',5025)
 serial_port = '/dev/ttyACM0'
 baud_rate = 115200
 
 freq = 2400
+start = 2000
+stop = 3000
 freq_units = 'MHz'
+
 avg_count = 2
 
+AntennaNum = 4
+Type = 'I'
+Voltage = 1.5
+
 # Initialize Network Analyzer
-pna = network_analyzer.network_analyzer(pna_address) # Create pna object
+pna = network_analyzer.network_analyzer(pna_address,False) # Create pna object
 
 # The following commands replace the initialize() function from the old calibration code:
 # Prepare the vna to read the phases from the markers
+
+# Marker num corresponds to trace num in this program
+s_param = "S12"
+mag_name = "mag_trace"
+mag_trace = 1
+
+phase_trace = 2
+phase_name = "phase_trace"
+
+window = 1
+
+
 pna.clear_calcs()
-pna.marker_on(1,freq,freq_units)
-pna.marker_on(2,freq,freq_units)
-pna.averaging_on('SWEEP',avg_count)
+
+pna.set_freq(start,stop,freq_units)
+
+pna.init_s_measurement(window,mag_name,mag_trace,s_param,"MLOG",freq,freq_units)
+pna.init_s_measurement(window, phase_name, phase_trace, s_param, "UPH", freq, freq_units, 500)
+
 
 # Initialize Serial Connection to Arduino
 ser_connection = serial.Serial(serial_port,baud_rate)
@@ -25,6 +50,19 @@ ser_connection = serial.Serial(serial_port,baud_rate)
 
 # After initialization, loop through different voltages to read phases
 
-# Set I voltage
+Voltage_List = []
+for i in range(5000):
+    # Dividing by 10 makes the floating point magic work out
+    Voltage_List.append(i/1000)
 
-# Read marker phase value
+print(Voltage_List)
+
+for Voltage in Voltage_List:
+    ser_connection.write(str(AntennaNum).encode())
+    ser_connection.write(str(Type).encode())
+    ser_connection.write(str(Voltage).encode())
+    # Set I voltage; 2 Seconds should be safe
+    sleep(2)
+    # Read marker phase value
+
+    print(Voltage," Volts ", pna.readMarker(phase_name,phase_trace)," Deg")
